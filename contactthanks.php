fa3f7d89b975939541fddbc68cbe907d0875d68e
doctypecode <67657284+doctypecode-wm@users.noreply.github.com>
2020-07-15 16:17:16 -0400

    Upload of Site
    First version of Personal Portfolio site. Was built with no CSS frameworks, and no JavaScript. All hand coded.

---
+++ contactthanks.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Joe Mancuso - Web Developer</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link rel="stylesheet" href="style2.css" type="text/css" />
+    <script
+      src="https://kit.fontawesome.com/0f1d600fe3.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+	  <div id="page-wrap">
+      <br /><br />
+      <h1>Your message has been sent!</h1><br />
+      <p><a href="index.html"><i class="fas fa-arrow-circle-left"></i> Back to the Website</a></p>
+    </div>
+    <footer>
+      <div class="footer-copy footer">
+        <span>Copyright &copy; 2020 Joe Mancuso</span>
+      </div>
+      <div class="footer-social footer">
+        <a href="https://github.com/doctypecode-wm" title="Follow me on Github!" target="_blank"><i class="fab fa-github fa-3x"></i></a>
+        <a href="https://www.linkedin.com/in/joe-mancuso-76593117/" title="Follow me on Linkedin!" target="_blank"><i class="fab fa-linkedin-in fa-3x"></i></a>
+      </div>
+    </footer>
+    <script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+    <script type="text/javascript"> _uacct = "UA-68528-29"; urchinTracker();</script>
+  </body>
+</html>
